248be72c566194a5ce1022828314078b76afa697
Anna Natorilla <annagaiel@gmail.com>
2016-06-02 21:26:50 -0700

    Add the unirest controller and the views for index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get '/', to: 'pdfs#index'
+  get '/:id', to: 'pdfs#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
