23b9dad3caa467987d3b9be2d28d515adfdf3f34
annayanushkevich <fishinmyhead@gmail.com>
2016-06-05 13:06:19 -0700

    added spacing

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -10,17 +10,19 @@ class PdfsController < ApplicationController
       format.html
       format.pdf do
         pdf = Prawn::Document.new
-        pdf.text @student["student"]["first_name"] + " " + @student["student"]["last_name"], size: 33, font: "Helvetica", color: "0000FF", align: :center
+        pdf.text @student["student"]["first_name"] + " " + @student["student"]["last_name"] + "
+        ", size: 33, font: "Helvetica", color: "0000FF", align: :center 
+        # url = "https://robohash.org/istevelitsit.png?size=300x300&set=set1"
+        # image open(url), fit: [200, 200], position: :center
       
         pdf.text "Email: " + @student["student"]["email"], align: :center, size: 18, font: "Helvetica"
-        pdf.text "Phone Number: " + @student["student"]["phone_number"], align: :center, size: 18, font: "Helvetica"
-      
-        pdf.text "Linkedin URL:" + @student["student"]["linkedin_url"], align: :right, size: 14, font: "Helvetica"
-        pdf.text "Twitter:" + @student["student"]["twitter_handle"], align: :right, size: 14, font: "Helvetica"
-        pdf.text "Wordpress:" + @student["student"]["wordpress_url"], align: :right, size: 14, font: "Helvetica"
-        pdf.text "Resume URL:" + @student["student"]["resume_url"], align: :right, size: 14, font: "Helvetica"
-        pdf.text "Github URL:" + @student["student"]["github_url"], align: :right, size: 14, font: "Helvetica"
-        pdf.text "BIO:" + @student["student"]["short_bio"],size: 20, font: "Helvetica", color: "660033", align: :center
+        pdf.text "Phone Number: " + @student["student"]["phone_number"] + "                                                             ", align: :center, size: 18, font: "Helvetica"
+        pdf.text "Linkedin URL:  " + @student["student"]["linkedin_url"], align: :right, size: 14, font: "Helvetica"
+        pdf.text "Twitter:  " + @student["student"]["twitter_handle"], align: :right, size: 14, font: "Helvetica"
+        pdf.text "Wordpress:  " + @student["student"]["wordpress_url"], align: :right, size: 14, font: "Helvetica"
+        pdf.text "Resume URL:  " + @student["student"]["resume_url"], align: :right, size: 14, font: "Helvetica"
+        pdf.text "Github URL:  " + @student["student"]["github_url"]+ "                                                                        ", align: :right, size: 14, font: "Helvetica"
+        pdf.text "BIO:  " + @student["student"]["short_bio"]+ "                                                             ",size: 20, font: "Helvetica", color: "660033", align: :center
 
         send_data pdf.render, type: "application/pdf", disposition:"inline"
       end
@@ -30,6 +32,6 @@ end
 
 
 # #   def header
-# #     image "(http://localhost:3001/api/v1/students/#{params[:id]})", width: 530, height: 150
+# #     image ("https://robohash.org/istevelitsit.png?size=300x300&set=set1"), width: 530, height: 150
 # #   end
 # # end
