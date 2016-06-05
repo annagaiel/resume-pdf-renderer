90367cb47fc775b63e399a9e8b207bf689fae296
annayanushkevich <fishinmyhead@gmail.com>
2016-06-05 10:35:48 -0700

    added styling

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -11,6 +11,7 @@ class PdfsController < ApplicationController
       format.pdf do
         pdf = Prawn::Document.new
         pdf.text @student["student"]["first_name"] + " " + @student["student"]["last_name"]
+      
         pdf.text "Email:" + @student["student"]["email"]
         pdf.text "Phone Number:" + @student["student"]["phone_number"]
         pdf.text "Linkedin URL:" + @student["student"]["linkedin_url"]
@@ -25,5 +26,28 @@ class PdfsController < ApplicationController
     end
   end
 
+  require "prawn"
 
+Prawn::Document.generate("http://localhost:3001/api/v1/students/#{params[:id]}.pdf") do
+ 
+  pdf.text @student["student"]["first_name"] + " " + @student["student"]["last_name"], size: 16, font: "Helvetica", color: "0000FF", align: :center
+  pdf.text "Phone Number:" + @student["student"]["phone_number"], align: :center
+
+  font_size 12
+  font "Courier" do
+    pdf.text "Email:" + @student["student"]["email"]
+    pdf.text "Linkedin URL:" + @student["student"]["linkedin_url"]
+    pdf.text "Twitter:" + @student["student"]["twitter_handle"]
+    pdf.text "Wordpress:" + @student["student"]["wordpress_url"]
+    pdf.text "Resume URL:" + @student["student"]["resume_url"]
+    pdf.text "Github URL:" + @student["student"]["github_url"]
+  end
+    
+    pdf.text "BIO:" + @student["student"]["short_bio"], size: 16, font: "Helvetica", color: "660033", align: :center
+  end
 end
+
+#   def header
+#     image "(http://localhost:3001/api/v1/students/#{params[:id]})", width: 530, height: 150
+#   end
+# end
