b7b0b3d73f0df195c3757ec8be21542ba5d3e564
Oliver <deftool@outlook.com>
2016-06-05 11:36:37 -0700

    fixed .body

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,26 +4,26 @@ class Student
 
   def initialize(hash)
     
-    @id = hash.body["student"]["id"]
-    @first_name = hash.body["student"]["first_name"]
-    @last_name = hash.body["student"]["last_name"]
-    @email = hash.body["student"]["email"]
-    @phone_number = hash.body["student"]["phone_number"]
-    @short_bio = hash.body["student"]["short_bio"]
-    @linkedin_url = hash.body["student"]["linkedin_url"]
-    @wordpress_url = hash.body["student"]["wordpress_url"]
-    @resume_url = hash.body["student"]["resume_url"]
-    @github_url = hash.body["student"]["github_url"]
-    @photo = hash.body["student"]["photo"]
+    @id = hash["student"]["id"]
+    @first_name = hash["student"]["first_name"]
+    @last_name = hash["student"]["last_name"]
+    @email = hash["student"]["email"]
+    @phone_number = hash["student"]["phone_number"]
+    @short_bio = hash["student"]["short_bio"]
+    @linkedin_url = hash["student"]["linkedin_url"]
+    @wordpress_url = hash["student"]["wordpress_url"]
+    @resume_url = hash["student"]["resume_url"]
+    @github_url = hash["student"]["github_url"]
+    @photo = hash["student"]["photo"]
   end
 
   def self.find(id)
-    student_hash = Unirest.get("#{ENV['DOMAIN']}/students/#{id}.json")
+    student_hash = Unirest.get("#{ENV['DOMAIN']}/students/#{id}.json").body
     return Student.new(student_hash)
   end
 
   def self.all
-    student_hashes = Unirest.get("#{ENV['DOMAIN']}/students.json")
+    student_hashes = Unirest.get("#{ENV['DOMAIN']}/students.json").body
     @students = []
     student_hashes.each do |hash|
       @student << Student.new(hash)
